8be97e21ce21b16fa49f5342336f23412e470bb8
mpyne <mpyne@protocase.com>
2025-02-19 14:23:06 -0400

    added some comments.

--- src/main/backups.ts
+++ src/main/backups.ts
@@ -12,7 +12,7 @@ export interface TaskSchedual {
 export interface BackUpTask {
   schedual: TaskSchedual
   clientID: string
-  description: string
-  source: string
-  target: string
+  description: string     // Unique description of the task. Also programatically add ID (Houston-backup-task) so we can query
+  source: string          // client folder to backup
+  target: string          // mount point for backup location(preappened clientID(client hostname))
 }
