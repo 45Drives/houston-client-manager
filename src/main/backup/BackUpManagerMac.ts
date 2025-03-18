5b3b8a579c7ca0f86a0e43ea39db5d28b796bd99
mpyne <mpyne@protocase.com>
2025-03-18 15:16:16 -0300

    added pop up before asking for user password to perform admin tasks.

--- src/main/backup/BackUpManagerMac.ts
+++ src/main/backup/BackUpManagerMac.ts
@@ -49,8 +49,9 @@ export class BackUpManagerMac implements BackUpManager {
       this.runAsAdmin(command);
     }
   }
-
-  private runAsAdmin(command: string): void {
+  
+  private runAsAdmin(command: string, message: string = "This Houston Client Manager requires administrator privileges.") : void {
+    execSync(`osascript -e 'display dialog "${message.replace(/"/g, '\\"')}" with title "Backup Scheduler" buttons {"OK"} default button 1'`);
     execSync(`osascript -e 'do shell script "${this.escapeForAppleScript(command)}" with administrator privileges'`);
   }
 
