aa11b5fa4a37e86f38cc2ab3053668d6e4a7e767
mpyne <mpyne@protocase.com>
2025-04-03 13:10:13 -0300

    added app logo. override sytle secondary button color.

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import "@45drives/houston-common-css/src/index.css"; 
 import "@45drives/houston-common-ui/style.css"; 
+import "./style.css"; 
 import App from './App.vue';
 
 const app = createApp(App);
