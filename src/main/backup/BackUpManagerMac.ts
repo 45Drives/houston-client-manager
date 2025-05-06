f41227bd6ba309f704511052d6d314fe89905cc0
JordanKeo45 <jkeough@45drives.com>
2025-05-06 11:36:24 -0300

    capitalizes D in 45Drives where appropriate

--- src/main/backup/BackUpManagerMac.ts
+++ src/main/backup/BackUpManagerMac.ts
@@ -64,7 +64,7 @@ export class BackUpManagerMac implements BackUpManager {
     throw new Error("Backup scheduling update is not supported on macOS yet.");
   }
 
-  private runAsAdmin(command: string, message: string = "This 45drives Setup Wizard requires administrator privileges."): void {
+  private runAsAdmin(command: string, message: string = "This 45Drives Setup Wizard requires administrator privileges."): void {
     execSync(`osascript -e 'display dialog "${message.replace(/"/g, '\\"')}" with title "Backup Scheduler" buttons {"OK"} default button 1'`);
     execSync(`osascript -e 'do shell script "${this.escapeForAppleScript(command)}" with administrator privileges'`);
   }
