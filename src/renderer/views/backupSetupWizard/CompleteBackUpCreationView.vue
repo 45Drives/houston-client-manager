7e077e6703704f75edca556043bb0f58c9ac1584
mpyne <mpyne@protocase.com>
2025-07-27 12:04:26 -0300

    fix new line encoding for setup-super-simple. added goto backup manager or setup after complete backup

--- src/renderer/views/backupSetupWizard/CompleteBackUpCreationView.vue
+++ src/renderer/views/backupSetupWizard/CompleteBackUpCreationView.vue
@@ -38,7 +38,7 @@
 
             <p class="mb-4 text-lg">NOTE: This computer and the server will have to turn on for backups to happen.</p>
 
-            <p class="mb-4 text-lg font-medium">Click "Finish" to complete the setup.</p>
+            <p class="mb-4 text-lg font-medium">Click "Go To Backup Manager" or "Setup More Storage Servers" to complete the setup.</p>
           </div>
         </div>
 
@@ -47,9 +47,15 @@
 
     <!-- Go to Home Button (visible once complete) -->
     <template #footer>
+
       <div class="button-group-row justify-end">
-        <button :disabled="setupComplete !== 'yes'" class="btn btn-primary w-40 h-20" @click="goHome">{{ "Finish!"
-          }}</button>
+
+        <button :disabled="setupComplete !== 'yes'" class="btn btn-primary w-40 h-20" @click="goToBackupWizard">{{
+          "Go To Backup Manager" }}</button>
+
+        <button :disabled="setupComplete !== 'yes'" class="btn btn-secondary w-40 h-20" @click="goToSetupWizard">{{
+          "Setup More Storage Servers" }}</button>
+
       </div>
     </template>
 
@@ -78,7 +84,7 @@ watch(setupComplete, (value) => {
   }
 });
 
-function goHome(): void {
+function goToBackupWizard(): void {
   // console.log("before clearing: ", backUpSetupConfig)
 
   if (backUpSetupConfig) {
@@ -96,6 +102,15 @@ function goHome(): void {
   // console.log("after clearing: ", backUpSetupConfig);
   reset();  // Assuming this is the wizard reset
 }
+
+
+function goToSetupWizard(): void {
+  IPCRouter.getInstance().send('renderer', 'action', JSON.stringify({
+    type: 'show_wizard',
+    wizard: 'storage'
+  }));
+}
+
 
 function handleActionEvent(data: string) {
   try {
@@ -148,13 +163,13 @@ onBeforeUnmount(() => {
 useEnterToAdvance(
   () => {
     if (setupComplete.value === "yes") {
-      goHome(); // Press Enter
+      goToBackupWizard(); // Press Enter
     }
   },
   300,
   () => {
     if (setupComplete.value === "yes") {
-      goHome(); // ArrowRight = Finish
+      goToBackupWizard(); // ArrowRight = Finish
     }
   },
   undefined // no need to handle ArrowLeft on this screen
