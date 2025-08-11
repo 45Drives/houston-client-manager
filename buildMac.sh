7df1e5d8381830e9cc09bb5d6439955cc52160dd
JordanKeo45 <jkeough@45drives.com>
2025-08-11 15:24:57 -0300

    fixing build script syntax issues

--- buildMac.sh
+++ buildMac.sh
@@ -24,20 +24,20 @@ echo '.....DONE.'
 
 echo '----- CODE SIGNING APP -----'
 ## Find all files and do deep code signing.
-find "$outputDir/$appName.app" -type f -exec codesign --deep -dvv --force --timestamp --options=runtime --entitlements $entitlementsFile --sign "$developerIdApplicationString" {} \;
+find "$outputDir/$appNameBase.app" -type f -exec codesign --deep -dvv --force --timestamp --options=runtime --entitlements $entitlementsFile --sign "$developerIdApplicationString" {} \;
 ## Code sign .app bundle
-codesign --deep -dvv --force --timestamp --options=runtime --entitlements $entitlementsFile --sign "$developerIdApplicationString" "$outputDir/$appName.app"
+codesign --deep -dvv --force --timestamp --options=runtime --entitlements $entitlementsFile --sign "$developerIdApplicationString" "$outputDir/$appNameBase.app"
 ## Verify code signing was successful.
-codesign --verify --strict -dvv "$outputDir/$appName.app"
+codesign --verify --strict -dvv "$outputDir/$appNameBase.app"
 echo '.....DONE.'
 
 ## Create a temp directory to add the symbolic link to the applications folder
 mkdir $outputDir/temp
-cp -R "$outputDir/$appName.app" $outputDir/temp
+cp -R "$outputDir/$appNameBase.app" $outputDir/temp
 ln -s /Applications $outputDir/temp/Applications
 
 echo '----- CREATING DMG -----'
-hdiutil create -volname "$appName" -srcfolder $outputDir/temp -ov -fs HFS+ -format UDZO -imagekey zlib-level=9 -o "$outputDir/$appName.dmg"
+hdiutil create -volname "$appNameBase" -srcfolder $outputDir/temp -ov -fs HFS+ -format UDZO -imagekey zlib-level=9 -o "$outputDir/$appName.dmg"
 echo '.....DONE.'
 
 echo '----- CODE SIGNING DMG -----'
