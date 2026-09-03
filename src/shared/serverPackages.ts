3280c9da698c10e1b487c4c93bb39b8495b5b432
JordanKeo45 <jkeough@45drives.com>
2026-09-03 10:21:28 -0300

    update min package version numbers

--- src/shared/serverPackages.ts
+++ src/shared/serverPackages.ts
@@ -20,8 +20,8 @@ export interface HoustonPackage {
 }
 
 export const HOUSTON_PACKAGES: readonly HoustonPackage[] = [
-  { name: 'houston-broadcaster', label: 'Houston Broadcaster', minVersion: '2.3.7-1' },
-  { name: 'cockpit-super-simple-setup', label: 'Super Simple Setup', minVersion: '1.2.2-1' },
+  { name: 'houston-broadcaster', label: 'Houston Broadcaster', minVersion: '2.3.8-1' },
+  { name: 'cockpit-super-simple-setup', label: 'Super Simple Setup', minVersion: '1.2.3-1' },
   { name: 'cockpit-scheduler', label: 'Task Scheduler', minVersion: '1.7.8-1' },
   { name: 'wireshield', label: 'WireShield', minVersion: '0.1.8' },
   { name: 'cockpit-zfs', label: 'ZFS Management' },
